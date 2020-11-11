e6cadc6f0a647429ede1aae0f3dd18729ae90887
Vini Rocha <vinirocha@Rocha-Server.local>
2020-11-10 22:50:10 -0800

    Move to MacBook Pro

--- src/lib/app.py
+++ src/lib/app.py
@@ -14,25 +14,34 @@ import datetime
 ONE_HOUR = 3600
 SCRIPT = 'send-message.applescript'
 DRIVER_PATH = '../web-driver/chromedriver'
-PRODUCT_URL = 'https://www.amway.com/en_US/Pursue%26trade%3B-Disinfectant-Deodorizer-Spray-%28ORDER-LIMIT-5%29-p-E0023?searchTerm=pursue'
-ELEMENT_XPATH = '/html/body/main/div[11]/div[2]/div[1]/div[2]/div/div[2]/div/div[2]/div[1]/div[1]/div/div/div[1]/div[2]/div/span/span[2]'
+# PRODUCT_URL = 'https://www.amway.com/en_US/Pursue%26trade%3B-Disinfectant-Deodorizer-Spray-%28ORDER-LIMIT-5%29-p-E0023?searchTerm=pursue'
+PRODUCT_URL = 'https://www.amway.com/en_US/Artistry-Ideal-Radiance%26trade%3B-Illuminating-CC-Cream---Light-Medium-p-118207?searchTerm=light%20medium'
 
-TODAYS_MESSAGE = """Product is in stock
-{}""".format(PRODUCT_URL)
+# CBD URL for TESTING
+# PRODUCT_URL = 'https://www.amway.com/en_US/XS%26trade%3B-CBD-Cream-p-296753?backRef=aHR0cHM6Ly93d3cuYW13YXkuY29tL2VuX1VTL3NlYXJjaC8/dGV4dD1jYmQ='
+ELEMENT_XPATH = '/html/body/main/div[11]/div[2]/div[1]/div[2]/div/div[2]/div/div[2]/div[1]/div[1]/div/div/div[1]/div[2]/div/span/span[2]'
+PRODUCT_NAME = 'Cream'
+TODAYS_MESSAGE = """{} is back in stock
+{}""".format(PRODUCT_NAME, PRODUCT_URL)
 
 CONTACT_LIST = {
     "ContactList": [
-        {"name": "Vini", "number": "17607990511"},
-        {"name": "Beth", "number": "17609025715"}
+        {"name": "Vini", "number": "17607990511"}
+        # ,
+        # {"name": "Beth", "number": "17609025715"}
     ]
 }
 
+print('Watching for {} \n'.format(PRODUCT_NAME))
+print('URL: {} \n'.format(PRODUCT_URL))
 
 # ================== Functions ==================
+
+
 def get_availability(url, xpath):
     # Set Setting for chrome
     chrome_options = Options()
-    chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--headless')
     driver = webdriver.Chrome(DRIVER_PATH, options=chrome_options)
 
     # Open Chrome on the URL
@@ -68,10 +77,12 @@ def send_message(data):
 
 
 # ================== App ==================
-status = 'Temporarily Out-of-Stock'
-while status == 'Temporarily Out-of-Stock':
-    time.sleep(ONE_HOUR)
-    print(datetime.datetime.now())
+out_of_stock = 'Temporarily Out-of-Stock'
+status = out_of_stock
+while status == out_of_stock:
+    print(datetime.datetime.now(), status)
     status = get_availability(PRODUCT_URL, ELEMENT_XPATH)
-    if status != 'Temporarily Out-of-Stock':
+    time.sleep(5)
+    # time.sleep(ONE_HOUR)
+    if status != out_of_stock:
         send_message(CONTACT_LIST)
