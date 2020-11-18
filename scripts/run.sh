3919cd13f88e2090f5481edfa1ae6fcf55dfa222
Vinicius Rocha <viniciusrocha@Vini-MacBook.local>
2020-11-17 22:12:25 -0800

    updated sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,5 @@
+# chmod +x /Users/viniciusrocha/development/VER-product-watcher/scripts/run.sh
+
 cd /Users/viniciusrocha/development/VER-product-watcher/src/lib/
 
 Python3 app.py Artistry-Ideal-Radiance https://www.amway.com/en_US/Artistry-Ideal-Radiance%26trade%3B-Illuminating-CC-Cream---Light-Medium-p-118207?searchTerm=light%20medium
